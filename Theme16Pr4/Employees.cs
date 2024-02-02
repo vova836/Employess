45c10752141f1315a578504201b89341a0eab18c
vova836 <123488703+vova836@users.noreply.github.com>
2024-02-02 13:54:16 +0200

    Add files via upload

---
+++ Theme16Pr4/Employees.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme16Pr4
+{
+    /// <summary>
+    /// Нестатический класс, описывающий сотрудников
+    /// </summary>
+    class Employees
+    {
+        /// <summary>
+        /// Поле табельного номера
+        /// </summary>
+        int employeeNumber;
+        /// <summary>
+        /// Поле фамилии 
+        /// </summary>
+        string surname;
+        /// <summary>
+        /// Поле имени
+        /// </summary>
+        string name;
+        /// <summary>
+        /// Поле отчества
+        /// </summary>
+        string patronymic;
+        /// <summary>
+        /// Поле даты рождения
+        /// </summary>
+        DateTime dateBirth;
+        /// <summary>
+        /// Поле должностии
+        /// </summary>
+        string position;
+        /// <summary>
+        /// Поле оклада
+        /// </summary>
+        int salary;
+
+        /// <summary>
+        /// Свойства полей
+        /// </summary>
+        public int EmployeeNumber { get => employeeNumber; set => employeeNumber = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public string Name { get => name; set => name = value; }
+        public string Patronymic { get => patronymic; set => patronymic = value; }
+        public DateTime DateBirth { get => dateBirth; set => dateBirth = value; }
+        public string Position { get => position; set => position = value; }
+        public int Salary { get => salary; set => salary = value; }
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Employees() { }
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="employeeNumber"></param>
+        /// <param name="surname"></param>
+        /// <param name="name"></param>
+        /// <param name="patronymic"></param>
+        /// <param name="dateBirth"></param>
+        /// <param name="position"></param>
+        /// <param name="salary"></param>
+        public Employees(int employeeNumber, string surname, string name, string patronymic, DateTime dateBirth, string position, int salary)
+        {
+            this.EmployeeNumber = employeeNumber;
+            this.Surname = surname;
+            this.Name = name;
+            this.Patronymic = patronymic;
+            this.DateBirth = dateBirth;
+            this.position = position;
+            this.salary = salary;
+        }
+        /// <summary>
+        /// Метод вывода информации о сотруднике
+        /// </summary>
+        public void Output()
+        {
+            if (Surname == "" || Name == "" || Patronymic == "" || Position == "")
+            {
+                Console.WriteLine("Отсутствует информация о сотруднике\n");
+            }
+            else
+            {
+                int age = DateTime.Now.Year - DateBirth.Year;
+                if (DateTime.Now.Month < dateBirth.Month) { age--; }
+                Console.WriteLine($"Табельный номер - {employeeNumber}\nФамилия - {surname}\nИмя - {name}\nОтчество - {patronymic}\nДата рождения - {dateBirth:d}\nВозраст сотрудника: {age} лет\nДолжность - {position}\nОклад - {salary}\n");
+
+            }
+        }
+    }
+}
---
+++ Theme16Pr4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme16Pr4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите кол-во сотрудников: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Employees[] empl = new Employees[n];
+            for (int i = 0; i< n; i++)
+            {
+                Console.Write("Введите табельный номер сотрудника: ");
+                int employeeNumber = int.Parse(Console.ReadLine());
+                Console.Write("Введите фамилию сотрудника: ");
+                string surname = Console.ReadLine();
+                Console.Write("Введите имя сотрудника: ");
+                string name = Console.ReadLine();
+                Console.Write("Введите отчество сотрудника: ");
+                string patronymic = Console.ReadLine();
+                Console.Write("Введите дату рождения сотрудника: ");
+                DateTime dateBirth = DateTime.Parse(Console.ReadLine());
+                Console.Write("Введите должность сотрудника: ");
+                string position = Console.ReadLine();
+                Console.Write("Введите оклад сотрудника: ");
+                int salary = int.Parse(Console.ReadLine());
+                empl[i] = new Employees(employeeNumber, surname, name, patronymic, dateBirth, position, salary);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Сотрудники: \n");
+            foreach (var emplo in empl)
+            {
+                Employees empl1 = new Employees(emplo.EmployeeNumber, emplo.Surname, emplo.Name, emplo.Patronymic, emplo.DateBirth, emplo.Position, emplo.Salary);
+                empl1.Output();
+
+            }    
+            //Console.WriteLine("Сотрудники: \n");
+            //foreach (var employee in empl)
+            //{
+            //    if (employee.EmployeeNumber == null || employee.Surname == "" || employee.Name == "" || employee.Patronymic == "" || employee.DateBirth == null || employee.Position == "" || employee.Salary == null)
+            //    {
+            //        Console.WriteLine("Отсутствует информация о сотруднике\n");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Табельный номер - {employee.EmployeeNumber}\nФамилия - {employee.Surname}\nИмя - {employee.Name}\nОтчество - {employee.Patronymic}\nДата рождения - {employee.DateBirth}\nДолжность - {employee.Position}\nОклад - {employee.Salary}\n");
+            //        int age = DateTime.Now.Year - employee.DateBirth.Year;
+            //        if (employee.DateBirth.Month<DateTime.Now.Month) { age--; }
+            //        Console.Write($"Возраст сотрудника: {age} лет");
+            //    }
+
+            //}
+            
+
+            Console.ReadKey();
+        }
+    }
+}
